e1a8628621211029a3f54d829c68799249561922
moradalkhatib <muradalkhateb@icloud.com>
2021-06-23 18:33:56 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ let leftIndex;
 let midIndex;
 let rightIndex;
 
-
 let imgArr = ['bag.jpg', 'banana.jpg', 'bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg',
 'chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg',
 'shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
@@ -24,24 +23,15 @@ function submitRound(event){
     round=document.getElementById('round').value;
     
 }
-
-
 formSubmit.addEventListener('submit',submitRound);
-
-
-function Images(name, imgSrc) {
+function Images(name, imgSrc ,views,clicked) {
     this.name = name;
-    this.imgSrc = `./images/${imgSrc}`;
-    this.view = 0;
-    this.click = 0;
+    this.imgSrc = imgSrc;
+    this.views = views;
+    this.clicked = clicked;
     Images.all.push(this);
 }
 Images.all = [];
-for (var i = 0; i < imgArr.length; i++) {
-    let imageName = imgArr[i].split('.')[0];
-    new Images(imageName, imgArr[i])
-}
-
 
 function renderImage(){
      
@@ -56,62 +46,49 @@ function renderImage(){
     
     
     compareArr=[];
-    compareArr.push(leftIndex);
-    compareArr.push(midIndex);
-    compareArr.push(rightIndex);
+    compareArr.push(leftIndex,midIndex,rightIndex);
+
     
     leftImg.src=Images.all[leftIndex].imgSrc;
     midelImg.src=Images.all[midIndex].imgSrc;
     rightImg.src=Images.all[rightIndex].imgSrc;
-    Images.all[leftIndex].view++;
-    Images.all[midIndex].view++;
-    Images.all[rightIndex].view++;
+    Images.all[leftIndex].views++;
+    Images.all[midIndex].views++;
+    Images.all[rightIndex].views++;
       
       
     
    
 }
-renderImage();
- 
 function clickFunction(event){
-    if((event.target.id === 'leftimg' || event.target.id === 'centerimg' || event.target.id === 'rightimg' )
-      &counter<round){
-        if(event.target.id === 'leftimg'){Images.all[leftIndex].click++}
-        if(event.target.id === 'centerimg'){Images.all[midIndex].click++}
-        if(event.target.id === 'rightimg'){Images.all[rightIndex].click++}
-        
+    if((event.target.id === 'leftimg' || event.target.id === 'centerimg' || event.target.id === 'rightimg') && counter < round)
+     {
+        if(event.target.id === 'leftimg'){Images.all[leftIndex].clicked++}
+        if(event.target.id === 'centerimg'){Images.all[midIndex].clicked++}
+        if(event.target.id === 'rightimg'){Images.all[rightIndex].clicked++}
         renderImage();
         counter++;
-
+        localStorage.setItem('order',JSON.stringify(Images.all));
     }
     if(counter==round){drawChart();}
-       
 }
 
 imageSection.addEventListener('click',clickFunction);
-
 function dataView() {
     document.getElementById('list').innerHTML='';
     for (let i = 0; i < imgArr.length; i++) {
       let item = document.createElement('li');
       list.appendChild(item);
-      item.textContent = `${Images.all[i].name} had ${Images.all[i].click} votes, and was seen ${Images.all[i].view} times.`;
+      item.textContent = `${Images.all[i].name} had ${Images.all[i].clicked} votes, and was seen ${Images.all[i].views} times.`;
       imageSection.removeEventListener('click',clickFunction);
-
-      
     }
     imageSection.addEventListener('click', clickFunction);
-    
+
   }
 
 viewResult.addEventListener('click',dataView);
-
-
 function getRandomNumber(min, max){
-    
     return Math.floor(Math.random() * (max - min + 1) + min); 
-      
-
 }
 
 function drawChart(){
@@ -121,13 +98,12 @@ function drawChart(){
   for(let i =0;i<imgArr.length;i++)
   {
     arrName.push(Images.all[i].name.split('.')[0]);
-    arrViews.push(Images.all[i].view);
-    arrVotes.push(Images.all[i].click);
-
+    arrViews.push(Images.all[i].views);
+    arrVotes.push(Images.all[i].clicked);
   }
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
       type: 'bar',
       data: {
           labels: arrName,
@@ -166,5 +142,20 @@ function drawChart(){
   
   
 }
-
-
+function getData(){
+    let data = JSON.parse(localStorage.getItem('order'));
+  
+    if(data){
+       for(let i=0;i<data.length;i++)  { 
+       new Images(data[i].name,data[i].imgSrc,data[i].views,data[i].clicked)
+    } 
+   
+    }else {
+        for (var i = 0; i < imgArr.length; i++) {
+            let imageName = imgArr[i].split('.')[0];
+            new Images(imageName, `./images/${imgArr[i]}`,0,0)
+        } 
+    }
+    renderImage();}
+  
+     getData();
